78ad966c62462cbf1fe0d3fec1dc5a23c0dfb66a
Tuanlt28 <lttuan28112002@gmail.com>
2023-10-01 22:42:50 +0700

    feat: commit init

---
+++ BE_SneakrerMisuShop/BE_SneakrerMisuShop/Bases/ResponseBase.cs
@@ -0,0 +1,8 @@
+﻿namespace BE_SneakrerMisuShop.Bases
+{
+    public class ResponseBase<T>
+    {
+        public T data { get; set; }
+        public int status { get; set; }
+    }
+}
--- BE_SneakrerMisuShop/BE_SneakrerMisuShop/Program.cs
+++ BE_SneakrerMisuShop/BE_SneakrerMisuShop/Program.cs
@@ -1,3 +1,7 @@
+using BE_SneakrerMisuShop.DatabaseContext;
+using Microsoft.EntityFrameworkCore;
+using System.Xml.Linq;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,6 +11,22 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("__cors", policy =>
+    {
+        policy.AllowAnyHeader()
+          .WithMethods("POST", "GET", "PUT", "PATCH", "DELETE")
+          .AllowCredentials()
+          .SetIsOriginAllowed(e => new Uri(e).Host == "localhost");
+    });
+});
+
+builder.Services.AddDbContext<MisuContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("SneakerMisuShop"));
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -15,6 +35,10 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseRouting();
+
+app.UseCors("__cors");
 
 app.UseHttpsRedirection();
 
